ba7b469a83b089686fca30f23f1752dddcdae8ea
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-26 22:04:59 +0200

    Created the styles

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("muie");
+let word = ["Alexandria"];
+let word = document.getElementById("word");
+let wordLength = word.length;
+let text = "_";
+for (let i = 0; i < wordLength; ++i) {
+    innerText += "_";
+}
+word.outerText = "text";
