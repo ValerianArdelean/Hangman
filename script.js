a8edf4718957525c0c985bbb0e0c91708bacf289
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-27 20:56:00 +0200

    Impelemented the functionality of the app

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-alert("muie");
-let word = ["Alexandria"];
+let find = "Alexandria";
+let live = 7;
+document.getElementById("lifes").innerText = `You have ${live} lives left`;
 let word = document.getElementById("word");
-let wordLength = word.length;
-let text = "_";
+let wordLength = find.length;
+let text = [];
 for (let i = 0; i < wordLength; ++i) {
-    innerText += "_";
+    text.push("_");
 }
-word.outerText = "text";
+word.innerText = text;
+function clickFunction() {
+    let letter = document.getElementById("letter").value;
+    let result = parseInt(find.indexOf(letter));
+    if (result !== -1) {
+        text[result] = find[result];
+        word.innerText = text;
+    } else {
+        --live;
+        document.getElementById("lifes").innerText = `You have ${live} lives left`;
+    }
+}
