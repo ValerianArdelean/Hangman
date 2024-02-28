2c364fa91411ec99ec9a551b0126d62c0d8dc9dd
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-28 21:29:23 +0200

    Implemented the "you lose" feature

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let find = ['A','l','e','x','a','n','d','r','i','a'];
-let live = 7;
-document.getElementById("lives").innerText = `*** ${live} *** lives left`;
+let wordToBeGuessed = ['A','l','e','x','a','n','d','r','i','a'];
+let wordLength = wordToBeGuessed.length;
+let lives = 7;
+function previewLives() {
+    document.getElementById("lives").innerText = `*** ${lives} *** lives left`;
+}
+previewLives();
+
 let word = document.getElementById("word");
-let wordLength = find.length;
 let text = [];
 for (let i = 0; i < wordLength; ++i) {
     text.push(" _ ");
@@ -13,16 +17,19 @@ function clickFunction() {
     let letter = document.getElementById("letter").value.toLowerCase();
     let found = false;
     for (let i = 0; i < wordLength; ++i) {
-        if (find[i].toLowerCase() === letter) {
+        if (wordToBeGuessed[i].toLowerCase() === letter) {
             found = true;
-            text[i] = ` ${find[i]} `;
+            text[i] = ` ${wordToBeGuessed[i]} `;
             word.innerText = text;
-            find[i] = '_';
+            wordToBeGuessed[i] = '_';
             break;
         }
     }
-    if (!found) {
-        --live;
-        document.getElementById("lives").innerText = `*** ${live} *** lives left`;
+    if (!found && lives) {
+        --lives;
+        previewLives();
+        if (parseInt(lives) === 0) {
+            document.getElementById("outcome").innerText = "You lost, please try again !";
+        }
     }
 }
