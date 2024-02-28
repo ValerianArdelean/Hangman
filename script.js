e24bd68309b407a1b440a2af06bbc34e3d7fbafc
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-29 00:38:18 +0200

    Refined Java Script
    changed variables and functions names and swap them to make more sense in code

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 let wordToBeGuessed = ['A','l','e','x','a','n','d','r','i','a'];
-let guessedLetters = [];
-let displayLetters = document.getElementById("word");
 let wordLength = wordToBeGuessed.length;
+let guessedLetters = [];
 let lives = 7;
 
-function createEmptyWord() {
+function createHorizontalLines() {
     for (let i = 0; i < wordLength; ++i) {
         guessedLetters.push(" _ ");
     }
 }
 
-function displayWord() {
-    displayLetters.innerText = guessedLetters;
+function displayLetters() {
+    let getWordPlace = document.getElementById("word");
+    getWordPlace.innerText = guessedLetters;
 }
 
 function displayLives() {
     document.getElementById("lives").innerText = `*** ${lives} *** lives left`;
 }
 
-createEmptyWord();
-displayWord();
+createHorizontalLines();
+displayLetters();
 displayLives();
 
 function processUserInput() {
     let letter = document.getElementById("letter").value;
     let letterFound = wordToBeGuessed.indexOf(letter);
+    let outcome = document.getElementById("outcome");
     if (parseInt(letterFound) !== -1 && lives && wordLength >= 0) {
         --wordLength;
         guessedLetters[letterFound] = ` ${letter} `;
         wordToBeGuessed[letterFound] = '_';
-        displayWord();
+        displayLetters();
         if (wordLength == 0) {
-            document.getElementById("outcome").innerText = "You win, CONGRATULATIONS !";
+            outcome.innerText = "You win, CONGRATULATIONS !";
         }
-    } else if (lives && wordLength > 0) {
+    } else if (lives && wordLength) {
         --lives;
         displayLives();
         if (parseInt(lives) === 0) {
-            document.getElementById("outcome").innerText = "You lost, please try again !";
+            outcome.innerText = "You lost, please try again !";
         }
     }
 }
