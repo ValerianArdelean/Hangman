f79962822acfc442c4d4448b2091d6e1145994ac
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-01-15 17:03:48 +0200

    Corect spacing errors

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 let wordToBeGuessed = ['A','l','e','x','a','n','d','r','i','a'];
 let wordLength = wordToBeGuessed.length;
-let guessedLetters = [];
-let lives = 7;
-
-function createHorizontalLines() {
-    for (let i = 0; i < wordLength; ++i) {
-        guessedLetters.push(" _ ");
-    }
-}
+let guessedLetters = Array(wordLength).fill(" _ ");
+let lives = wordLength;
 
 function displayLetters() {
     let getWordPlace = document.getElementById("word");
@@ -18,7 +12,6 @@ function displayLives() {
     document.getElementById("lives").innerText = `*** ${lives} *** lives left`;
 }
 
-createHorizontalLines();
 displayLetters();
 displayLives();
 
@@ -26,7 +19,7 @@ function processUserInput() {
     let letter = document.getElementById("letter").value;
     let letterFound = wordToBeGuessed.indexOf(letter);
     let outcome = document.getElementById("outcome");
-    if (parseInt(letterFound) !== -1 && lives && wordLength >= 0) {
+    if (parseInt(letterFound) !== -1 && lives) {
         --wordLength;
         guessedLetters[letterFound] = ` ${letter} `;
         wordToBeGuessed[letterFound] = '_';
@@ -34,7 +27,7 @@ function processUserInput() {
         if (wordLength == 0) {
             outcome.innerText = "You win, CONGRATULATIONS !";
         }
-    } else if (lives && wordLength) {
+    } else if (lives) {
         --lives;
         displayLives();
         if (parseInt(lives) === 0) {
