7d8bf5defed25cfdc3ac1350f4068388e4212274
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-01-15 18:55:49 +0200

    Refine code

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
+const MAX_LIVES = 7;
+let livesCount = MAX_LIVES;
 let wordToBeGuessed = ['A','l','e','x','a','n','d','r','i','a'];
 let wordLength = wordToBeGuessed.length;
 let guessedLetters = Array(wordLength).fill(" _ ");
-let lives = wordLength;
-
-function displayLetters() {
-    let getWordPlace = document.getElementById("word");
-    getWordPlace.innerText = guessedLetters;
-}
-
-function displayLives() {
-    document.getElementById("lives").innerText = `*** ${lives} *** lives left`;
-}
-
-displayLetters();
-displayLives();
+let statusInfo = document.getElementById("lives");
+statusInfo.innerText = `*** ${livesCount} *** lives left`;
+let getWordPlace = document.getElementById("word");
+getWordPlace.innerText = guessedLetters;
 
 function processUserInput() {
     let letter = document.getElementById("letter").value;
     let letterFound = wordToBeGuessed.indexOf(letter);
     let outcome = document.getElementById("outcome");
-    if (parseInt(letterFound) !== -1 && lives) {
+    if (parseInt(letterFound) !== -1 && livesCount) {
         --wordLength;
         guessedLetters[letterFound] = ` ${letter} `;
         wordToBeGuessed[letterFound] = '_';
-        displayLetters();
+		getWordPlace.innerText = guessedLetters;
         if (wordLength == 0) {
-            outcome.innerText = "You win, CONGRATULATIONS !";
+			statusInfo.innerText = "You win, CONGRATULATIONS !";
         }
-    } else if (lives) {
-        --lives;
-        displayLives();
-        if (parseInt(lives) === 0) {
-            outcome.innerText = "You lost, please try again !";
+    } else if (livesCount && wordLength > 0) {
+        --livesCount;
+		statusInfo.innerText = `*** ${livesCount} *** lives left`;
+        if (parseInt(livesCount) === 0) {
+			statusInfo.innerText = "You lost, please try again !";
         }
     }
 }
